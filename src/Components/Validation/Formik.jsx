924c55da04b5e48962e32389756ed2e8b544c715
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-21 01:14:51 +0530

    feat - formik functionality added

---
+++ src/Components/Validation/Formik.jsx
@@ -0,0 +1,45 @@
+import { useFormik } from 'formik'
+import React from 'react'
+
+const Formik = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            userName: "",
+            password: "",
+            city: ""
+        },
+        onSubmit: (values) => {
+            alert(JSON.stringify(values))
+        }
+    })
+    return (
+        <div className='bg-dark text-white vh-100'>
+            <form onSubmit={formik.handleSubmit}>
+                <dl>
+                    <dt>User Name</dt>
+                    <dd>
+                        <input className='form-control w-25' type="text" name='userName'
+                            value={formik.values.userName} onChange={formik.handleChange} />
+                    </dd>
+                    <dt>Password</dt>
+                    <dd>
+                        <input className='form-control w-25' type="password"
+                            name='password' value={formik.values.password} onChange={formik.handleChange} />
+                    </dd>
+                    <dt>City</dt>
+                    <dd>
+                        <select className='form-select w-25' name='city' value={formik.values.city} 
+                        onChange={formik.handleChange}>
+                            <option>Pune</option>
+                            <option>Nagpur</option>
+                        </select>
+                    </dd>
+                </dl>
+                <button className='btn btn-primary'>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Formik
