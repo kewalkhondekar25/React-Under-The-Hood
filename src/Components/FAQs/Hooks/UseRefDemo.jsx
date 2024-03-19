2b957ff3964f3fd4f1e20364c6305e33ed5913cf
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-19 17:43:39 +0530

    useRef current added

--- src/Components/FAQs/Hooks/UseRefDemo.jsx
+++ src/Components/FAQs/Hooks/UseRefDemo.jsx
@@ -2,16 +2,18 @@ import React, { useEffect, useRef } from 'react'
 
 const UseRefDemo = () => {
     const cursor = useRef(null);
+    const btn = useRef(null);
 
     useEffect(() => {
         cursor.current.focus();
+        console.log(btn.current);
     }, [])
   return (
     <div className='bg-dark text-white vh-100'>
         <dl>
             <dt>Login</dt>
             <dd><input className='form-control w-25' ref={cursor} type="text" placeholder='Enter Your User Id '/></dd>
-            <button className='btn btn-primary w-25'>Log In</button>
+            <button className='btn btn-primary w-25' ref={btn}>Log In</button>
         </dl>
     </div>
   )
